e632cbdf4058c7de9c43ca0c7845b7c4fd7cc83d
Mauville <4991127@outlook.com>
2021-11-16 01:15:55 -0600

    Add video page

--- src/client/src/components/pages/VideoPage.js
+++ src/client/src/components/pages/VideoPage.js
@@ -14,13 +14,45 @@ Noviembre 14, 2021
 –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 */
 
+
 // Importar Componentes
 import Page from './Page'
+import {Space, Radio} from 'antd'
+import {DislikeOutlined, LikeOutlined} from '@ant-design/icons';
+import "../css/VideoPage.css";
+
 
 export default function VideoPage() {
+    // var video = localStorage.getItem('video')
+    const video = {
+        url: "https://www.youtube.com/watch?v=Q4MymPStabI",
+        titulo: "Blind (Frankie Knuckles Remix)",
+        descripcion: "Provided to YouTube by Parlophone UK"
+    }
     return (
-        <Page id="VideoPage" selectedKey={2}>
-            <h1 align="center">Video</h1>
+        <Page id="VideoPage" classname="" selectedKey={3}>
+            <Space id="video-container" direction="vertical">
+                <iframe
+                    width="960" height="615"
+                    src="https://www.youtube.com/embed/Q4MymPStabI"
+                    // src={video.url}
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
+                <Space direction="horizontal" id='titlebar'>
+                    <h1>{video.titulo}</h1>
+                    <Radio.Group>
+                        <Radio.Button value="large">
+                            <LikeOutlined/>
+                        </Radio.Button>
+                        <Radio.Button value="small">
+                            <DislikeOutlined/>
+                        </Radio.Button>
+                    </Radio.Group>
+                </Space>
+                <p>{video.descripcion}</p>
+                {/*<p>Description text goes here</p>*/}
+            </Space>
         </Page>
     )
 }
